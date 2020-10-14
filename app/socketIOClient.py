9a2ff25033942364f02d0a89ba803cb6b4129b6c
alextay96 <alextay96@alextay96-K55VD.realtek>
2020-10-14 12:50:01 +0800

    Added endpoint health status checking and pm2 auto start

--- app/socketIOClient.py
+++ app/socketIOClient.py
@@ -4,15 +4,19 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad, pad
 from base64 import b64decode, b64encode
 from Crypto.Random import get_random_bytes
-def welcome(data ):
+
+
+def decryption(data):
     print('welcome received', data)
     payloadSlice = data.split("*")
-    decrypted = str(decrypt(payloadSlice[2], payloadSlice[1], payloadSlice[0]), "utf8")
-    socketIO.emit("ccc", decrypted)
+    decrypted = str(
+        decrypt(payloadSlice[2], payloadSlice[1], payloadSlice[0]), "utf8")
+    socketIO.emit("status_update", decrypted)
 
-def www(a,b,c ):
-    print('welcome received', data)
-    
+
+def keep_alive(data):
+    socketIO.emit("gateway_status", "alive")
+
 
 def decrypt(data_element, iv, api_key):
 
@@ -29,6 +33,12 @@ def decrypt(data_element, iv, api_key):
     plaintext = unpad(padded, AES.block_size)
 
     return plaintext
-socketIO = SocketIO('http://localhost:8765', verify=True, wait_for_connection=True)
-socketIO.on('aaa', welcome)
+
+
+socketIO = SocketIO('http://localhost:7878', verify=True,
+                    wait_for_connection=True)
+
+socketIO.on('decrypt', decryption)
+socketIO.on('alive', keep_alive)
+
 socketIO.wait()
